c50d30d95640dc9366a967819b9f448025ba541f
Erick Gabriel <48528751+erickamaroamaro@users.noreply.github.com>
2026-07-16 21:33:57 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String sReverse = new StringBuilder(s).reverse().toString();
+
+        return s.equals(sReverse); 
+    }
+}
